3cdcb98a00d1d321b5f3a3232e40050479b4fb1d
Dachi301 <qirikashvili2020@gmail.com>
2023-03-15 17:22:22 +0400

    feat: added localstorage

--- src/context/cart.tsx
+++ src/context/cart.tsx
@@ -1,13 +1,32 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import dynamic from "next/dynamic";
+import useLocalStorage from "@/hooks/useLocalStorage";
 
 // Creating the context object and passing the default values.
 
 export const CartArr = createContext([]);
 
-function CartContext({ children }: any) {
-    const [cart, setCart] = useState([]);
+dynamic(() => require("@/hooks/useLocalStorage"), {
+    ssr: false
+});
 
-    return <CartArr.Provider value={{ cart, setCart }}>{children}</CartArr.Provider>;
+function CartContext({ children }: any) {
+    const [cart, setCart] = useLocalStorage("data", []);
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    return (
+        <>
+            {isMounted ? (
+                <CartArr.Provider value={{ cart, setCart }}>{children}</CartArr.Provider>
+            ) : (
+                <div>Loading...</div>
+            )}
+        </>
+    );
 }
 
 export default CartContext;
