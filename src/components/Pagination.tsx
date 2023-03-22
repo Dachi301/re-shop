a45925a576a7764050ce87d09bfde8800dc054a7
Dachi301 <qirikashvili2020@gmail.com>
2023-03-22 16:04:25 +0400

    style: added some styles on pagination

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Pagination = ({
     postsPerPage,
@@ -13,18 +13,28 @@ const Pagination = ({
     for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
         pageNumbers.push(i);
     }
+
+    const [active, setActive] = useState(0);
+
+    useEffect(() => {
+        console.log(active);
+    }, [active]);
+
     return (
         <nav className={" flex justify-center"}>
             <ul className={"flex gap-5"}>
-                {pageNumbers.map(number => (
+                {pageNumbers.map((number, id) => (
                     <li key={number}>
                         <button
-                            className={"bg-[gray] px-[10px] py-[3px] rounded-[50%] text-[#FFF]"}
-                            onClick={() => {
+                            id={`${id}`}
+                            className={`px-[10px] py-[3px] rounded-[50%] text-black hover:bg-[#e7c128] ${
+                                active === Number(id) ? "bg-[#e7c128]" : "bg-[gray]"
+                            }`}
+                            onClick={e => {
                                 paginate(number);
-                            }}
-                            // href="#">
-                        >
+
+                                setActive(Number(e.target.id));
+                            }}>
                             {number}
                         </button>
                     </li>
